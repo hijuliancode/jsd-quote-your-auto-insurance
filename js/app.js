1c27d5938408f502c91072aea22cbeb21cbe1e29
JulianCode <hijuliansosa@protonmail.com>
2020-05-19 14:18:52 -0500

    Reading the form data

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 // Variables
 const selectAnios = document.getElementById('anio'),
       max = new Date().getFullYear(),
-      min = max - 20;
+      min = max - 20,
+      formulario = document.getElementById('cotizar-seguro');
+
+// Constructores
+function Seguro(marca, anio, tipo) {
+  this.marca = marca;
+  this.anio = anio;
+  this.tipo = tipo;
+}
+function Interfaz() {}
 
 // Listeners
 eventListeners()
 function eventListeners() {
   document.addEventListener('DOMContentLoaded', iniciarApp)
+  formulario.addEventListener('submit', enviarFormulario);
 }
 
 // Funciones
@@ -21,5 +31,25 @@ function cargarAnios() {
         option.innerHTML = i;
 
     selectAnios.appendChild(option)
+  }
+}
+function enviarFormulario(e) {
+  e.preventDefault();
+
+  const marca = document.getElementById('marca'),
+        marcaSeleccionada = marca.options[marca.selectedIndex].value;
+
+  const anio = document.getElementById('anio'),
+        anioSeleccionado = anio.options[anio.selectedIndex].value;
+
+  const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+  // Crear instancía de interfaz
+  const interfaz = new Interfaz();
+  if(marcaSeleccionada == '' || anioSeleccionado == '' || tipo == '') {
+    // Interfaz Imprimiendo Error
+    console.log('Faltan Datos')
+  } else {
+    // Instanciar seguro y mostrar interfaz
   }
 }
