ce5dd8bd99bd4dbd9e5e65895c5a2ecad9b9c2c7
JulianCode <hijuliansosa@protonmail.com>
2020-05-19 19:08:25 -0500

    Converting Project from Prototypes to Classes

---
+++ js/app-es6.js
@@ -0,0 +1,159 @@
+// Variables
+const selectAnios = document.getElementById('anio'),
+      max = new Date().getFullYear(),
+      min = max - 20,
+      formulario = document.getElementById('cotizar-seguro');
+
+// Constructores
+class Seguro {
+  constructor(marca, anio, tipo) {
+    this.marca = marca;
+    this.anio = anio;
+    this.tipo = tipo;
+  }
+
+  cotizarSeguro() {
+    const { marca, anio, tipo } = this;
+    /**
+     * 1 = americano 1.15
+     * 2 = asiatico 1.05
+     * 3 = europeo 1.35
+     */
+    let cantidad;
+    const base = 2000;
+    switch(marca) {
+      case '1':
+        cantidad = base * 1.15
+        break
+      case '2':
+        cantidad = base * 1.05
+        break
+      case '3':
+        cantidad = base * 1.35
+        break
+    }
+  
+    // Leer y calcular diferencia de años
+    const diferencia = new Date().getFullYear() - anio;
+    // Cada año de diferencia hay que reducir el 3% del seguro
+    cantidad -= ((diferencia * 3) * cantidad) / 100;
+  
+    /**
+     * Si el seguro es básico se multiplica por 30% más
+     * Si el seguro es completo se multiplica por 50% más
+     */
+    (tipo === 'basico')
+      ? cantidad *= 1.30
+      : cantidad *= 1.50;
+  
+      console.log(cantidad)
+      return cantidad;
+  }
+}
+
+
+class Interfaz {
+  // Mensaje que se imprime en el html
+  mostrarMensaje(mensaje, tipo) {
+    const div = document.createElement('div');
+
+    (tipo === 'error')
+      ? div.classList.add('mensaje', 'error')
+      : div.classList.add('mensaje', 'correcto');
+
+    div.innerHTML = `${mensaje}`
+    formulario.insertBefore(div, document.querySelector('.form-group'))
+    setTimeout(() => {
+      document.querySelector('.mensaje').remove()
+    }, 3000);
+  }
+
+  // Imprime el resultado de la cotización
+  mostrarResultado(seguro, total) {
+    const resultadoElm = document.getElementById('resultado')
+    const { anio, tipo } = seguro
+    let marca;
+    switch(seguro.marca) {
+      case '1':
+        marca = 'Americano'
+        break
+      case '2':
+        marca = 'Asiatico'
+        break
+      case '3':
+        marca = 'Europeo'
+        break
+    }
+    const div = document.createElement('div')
+    div.innerHTML = `
+      <p class='header'>Tu resumen: </p>
+      <p><strong>Marca: </strong> ${marca}</p>
+      <p><strong>Año: </strong> ${anio}</p>
+      <p><strong>Tipo: </strong> ${tipo}</p>
+      <p><strong>Total: </strong> ${total}</p>
+    `;
+  
+    const spinner = document.querySelector('#cargando img')
+    spinner.style.display = 'block'
+    setTimeout(() => {
+      spinner.style.display = 'none'
+      resultadoElm.appendChild(div)
+    }, 2500);
+  
+  }
+}
+
+// Listeners
+eventListeners()
+function eventListeners() {
+  document.addEventListener('DOMContentLoaded', iniciarApp)
+  formulario.addEventListener('submit', enviarFormulario);
+}
+
+// Funciones
+function iniciarApp() {
+  console.log('iniciarApp')
+  cargarAnios()
+}
+
+function cargarAnios() {
+  for (let i = max; i >= min; i--) {
+    let option = document.createElement('option');
+        option.value = i;
+        option.innerHTML = i;
+
+    selectAnios.appendChild(option)
+  }
+}
+
+function enviarFormulario(e) {
+  e.preventDefault();
+
+  const marca = document.getElementById('marca'),
+        marcaSeleccionada = marca.options[marca.selectedIndex].value;
+
+  const anio = document.getElementById('anio'),
+        anioSeleccionado = anio.options[anio.selectedIndex].value;
+
+  const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+  // Crear instancía de interfaz
+  const interfaz = new Interfaz();
+  if(marcaSeleccionada == '' || anioSeleccionado == '' || tipo == '') {
+    // Interfaz Imprimiendo Error
+    interfaz.mostrarMensaje('Faltan datos, revisa el formulario y prueba de nuevo', 'error')
+  } else {
+    // Limpiar resultados anteriores
+    const resultados = document.querySelector('#resultado div')
+    if (resultados !== null) {
+      resultados.remove()
+    }
+    // Instanciar seguro y mostrar interfaz
+    const seguro = new Seguro(marcaSeleccionada, anioSeleccionado, tipo)
+    // cotizar seguro
+    const cantidad = seguro.cotizarSeguro()
+    // Mostrar el resultado
+    interfaz.mostrarResultado(seguro, cantidad)
+    interfaz.mostrarMensaje('Cotizando', 'correcto')
+  }
+}
